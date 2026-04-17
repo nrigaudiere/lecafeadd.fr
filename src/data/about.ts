b0fcaa9183592f4e635c02135eba90d938e9e7fe
Nicolas Rigaudiere <i.am.a@curious.engineer>
2026-04-18 00:04:18 +0200

    feat: add timeline

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,3 +1,5 @@
+import type { TimelineItem } from '@types/Timeline.type'
+
 export const aboutDetails = {
   title: 'A propos',
   description: `L'association CAFE ASSOCIATIF DE DROUE SUR DROUETTE a pour objet de :
@@ -8,4 +10,21 @@ export const aboutDetails = {
   - Proposer aux associations, aux artistes, …un lieu d'épanouissement, d'activités et d'initiatives.
   - Valoriser les activités locales (artisans, commerce, agriculture…) et toute activité d’intérêt général avec des partenaires de proximité.`,
   imgUrl: '/assets/images/about.png',
+  timelineItems: [
+    {
+      date: '22 mars 2024',
+      title: 'AG Constitutive',
+      description: "Création de l'association du café associatif de Droue sur Drouette",
+    },
+    {
+      date: '22 janvier 2025',
+      title: 'Début des travaux',
+      description: 'Les travaux de création du café sont démarrés afin de préparer le lieu fourni par la mairie.',
+    },
+    {
+      date: '05 octobre 2025',
+      title: 'Journée portes ouvertes',
+      description: "L'association est présentée aux adhérents ainsi qu'aux habitants du village.",
+    },
+  ] satisfies TimelineItem[],
 }
