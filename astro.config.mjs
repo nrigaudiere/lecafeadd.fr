2ac35a37a9d782b801b7a435a4f458816b05189b
Nicolas Rigaudiere <i.am.a@curious.engineer>
2026-04-17 23:57:40 +0200

    chore: tailwind 4 daisyui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
 import lit from '@astrojs/lit'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lecafeadd.fr/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  sitemap: true, // Generate sitemap (set to "false" to disable)
-  integrations: [tailwind(), sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  site: 'https://lecafeadd.fr/',
+
+  // Generate sitemap (set to "false" to disable)
+  sitemap: true,
+
+  // Add renderers to the config
+  integrations: [sitemap(), mdx(), lit(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
